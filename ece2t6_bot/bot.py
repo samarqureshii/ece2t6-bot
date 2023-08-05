f0e937aec5a6b1a31ff9ed6d6bc13b0d8824aa39
Agrim <agrimsharma2004@gmail.com>
2023-08-05 02:47:26 -0700

    tried using app_commands over discord.ext

--- ece2t6_bot/bot.py
+++ ece2t6_bot/bot.py
@@ -1,23 +1,33 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 import logging
 
-bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())  
 logger = logging.getLogger(__name__)
 
+# setting which events the bot should listen for
+intents = discord.Intents.default()
 
-@bot.event
+# creating a bot client with intents
+client = discord.Client(intents=intents)
+
+# a container that holds a list of all of the bots commands
+tree = app_commands.CommandTree(client)
+
+@tree.command(name = "ping", description= "test")
+async def ping(interaction):
+    await interaction.response.send_message("pong")
+
+@client.event
 async def on_ready():
-    logger.info(f'We have logged in as {bot.user}!')
-
-    synced = await bot.tree.sync()
-    logger.info('Synced commands: ' + ', '.join([f'/{cmd.name}' for cmd in synced]))
-
-
-@bot.tree.command()
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong!")
-
+    '''
+    On startup, the bot collects all of the coded commands.
+    It then syncs it to discord for users to use.
+    '''
+    await tree.sync()
+    print("Ready!")
 
 def run_bot(token: str):
-    bot.run(token, log_handler=None)
+    '''
+    Runs the client.
+    '''
+    client.run(token, log_handler=None)
