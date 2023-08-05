97a7858c97c39a7ea030ade90f57c67dfd472cc5
jdtech3 <dm.joedai@gmail.com>
2023-08-05 08:46:40 +0000

    Slightly better logging

--- ece2t6_bot/bot.py
+++ ece2t6_bot/bot.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
+import logging
 
 bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())  
+logger = logging.getLogger(__name__)
 
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    logger.info(f'We have logged in as {bot.user}!')
 
 
 @bot.command()
@@ -15,4 +17,4 @@ async def ping(ctx):
 
 
 def run_bot(token: str):
-    bot.run(token)
+    bot.run(token, log_handler=None)
--- run.py
+++ run.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 import logging
 import configparser
 import os
+import discord
 
 import ece2t6_bot.bot as bot
 
 
-def _load_config(path: str = 'config.ini'):
+def _load_config(path: str = 'config.ini') -> configparser.ConfigParser:
     config = configparser.ConfigParser()
 
     if not os.path.isfile(path):
@@ -18,22 +19,26 @@ def _load_config(path: str = 'config.ini'):
     return config
 
 
-def _setup_logging(log_level: int | str):
+def _setup_logging(log_level: int | str) -> logging.Logger:
     if isinstance(log_level, str):
         log_level = logging.getLevelName(log_level)
 
-    logging.basicConfig(
-        level=log_level,
-        format='[%(asctime)s] [%(module)s] %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = discord.utils._ColourFormatter()    # shamelessly stealing Discord's formatter
+
+    handler.setFormatter(formatter)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+
+    return logging.getLogger('launcher')    # yes, separation of concerns, but this is convenient
 
 
 def run():
     config = _load_config()
-    _setup_logging(config['Launcher']['LogLevel'])
+    logger = _setup_logging(config['Launcher']['LogLevel'])
 
-    logging.info('ece2t6-bot launching!')
+    logger.info('ece2t6-bot launching!')
 
     bot.run_bot(config['Discord']['Token'])
 
