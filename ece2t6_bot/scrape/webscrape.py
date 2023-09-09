d694aed7de45042678bc70c9494bd6123e035ebb
jdtech3 <dm.joedai@gmail.com>
2023-09-09 01:53:07 +0000

    Cleanup webscrape script: use datetime.time objs, get weekday names

--- ece2t6_bot/scrape/webscrape.py
+++ ece2t6_bot/scrape/webscrape.py
@@ -1,26 +1,37 @@
+import json
+from json.decoder import JSONDecodeError
+import datetime
+import calendar
+import sys
+
 import aiohttp
 import asyncio
-import json
+
 
 def set_payload(payload_file: str) -> dict:
     try:
         with open(payload_file, "r") as payload:
             data = json.load(payload)
-    except:
-        print(f"{payload_file} isn't formatted correctly.")
+    except JSONDecodeError as e:
+        print(f"{payload_file} isn't formatted correctly: {e}")
+        print("Exiting!")
+        sys.exit(-1)
 
     return data
+
 
 def organize_data(data_file: str) -> dict:
     with open(data_file, "r") as f:
         data = json.load(f)
+
+    dummy_timestamp = datetime.datetime.fromtimestamp(0)
 
     for course in data["payload"]["pageableCourse"]["courses"]:
         id = course["id"]
         name = course["name"]
         code = course["code"]
         section_count = len(course["sections"])
-        
+
         print(f"ID: {id}, NAME: {name}, CODE: {code}, SECTIONS: {section_count}")
 
         for section in course["sections"]:
@@ -30,21 +41,21 @@ def organize_data(data_file: str) -> dict:
             section_type = section["type"]
             section_number = section["sectionNumber"]
             # change up indicies
-            location = f'{section["meetingTimes"][0]["building"]["buildingCode"]} {section["meetingTimes"][0]["building"]["buildingRoomNumber"]}'
-            meeting_day = section["meetingTimes"][0]["start"]["day"]
-            start_time = section["meetingTimes"][0]["start"]["millisofday"] / 3600000
-            end_time = section["meetingTimes"][0]["end"]["millisofday"] / 3600000
+            datetime.datetime.fromtimestamp(0)
+            location = f'{section["meetingTimes"][0]["building"]["buildingCode"]}'
+            meeting_day = calendar.day_name[section["meetingTimes"][0]["start"]["day"]]
+            start_time = (dummy_timestamp + datetime.timedelta(milliseconds=section["meetingTimes"][0]["start"]["millisofday"])).time()
+            end_time = (dummy_timestamp + datetime.timedelta(milliseconds=section["meetingTimes"][0]["end"]["millisofday"])).time()
 
             print(f"{instructors} {section_name} {section_type} {section_number} {location} {meeting_day} {start_time} {end_time}")
 
-async def main():
 
+async def main():
     url = 'https://api.easi.utoronto.ca/ttb/getPageableCourses'
     headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        }
-    
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 
     print("Getting payload...")
 
@@ -76,7 +87,7 @@ async def main():
                 json.dump(data, f, indent=4)
 
     print("Organizing data.")
-    
+
     organize_data("data.json")
 
     print("Data organized.")
